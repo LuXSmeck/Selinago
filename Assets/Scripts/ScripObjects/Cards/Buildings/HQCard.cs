9ceb03efcf042a987f408cc2c585248aeae08314
LuXSmeck <smeckhell@hotmail.de>
2024-01-15 13:53:35 +0100

    Upload_HQ
    Start with the HQ Building

---
+++ Assets/Scripts/ScripObjects/Cards/Buildings/HQCard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Cards/Building/HQ", fileName = "Building")]
+public class HQCard: FactoryCard {
+   
+   //************************************************************************************************* Getter & Setters
+   
+}
